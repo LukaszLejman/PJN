0f86ef49d3a5d57675cc84e0b74b1dde2a7e6b71
LukaszLejman <LukaszLejman96@wp.pl>
2017-12-08 14:18:36 +0100

    2

--- pjn.py
+++ pjn.py
@@ -3,6 +3,8 @@ tmpHR = ''
 tmpENG = ''
 en = []
 cr = []
+open('EN.txt', 'w').close()
+open('HR.txt', 'w').close()
 for i in range(1,3):
     tmpENG = open('manuals/' + str(i) + '/english.txt', encoding='utf-8').read()
     tmpHR = open('manuals/' + str(i) + '/croatian.txt', encoding='utf-8').read()
