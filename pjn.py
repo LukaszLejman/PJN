3bb1ddce4be4a10a5bf495541a074fe37bddc573
LukaszLejman <LukaszLejman96@wp.pl>
2017-12-08 14:06:42 +0100

    1

---
+++ pjn.py
@@ -0,0 +1,12 @@
+from nltk.tokenize import sent_tokenize
+tmpHR = ''
+tmpENG = ''
+en = []
+cr = []
+for i in range(1,3):
+    tmpENG = open('manuals/' + str(i) + '/english.txt', encoding='utf-8').read()
+    tmpHR = open('manuals/' + str(i) + '/croatian.txt', encoding='utf-8').read()
+    with open("EN.txt", "a", encoding='utf-8') as myfile:
+        myfile.write(tmpENG)
+    with open("HR.txt", "a", encoding='utf-8') as myfile:
+        myfile.write(tmpHR)
