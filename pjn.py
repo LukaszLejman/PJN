571762088439c70998b217075261041e8d5b2bec
LukaszLejman <LukaszLejman96@wp.pl>
2017-12-09 22:59:02 +0100

    2

--- pjn.py
+++ pjn.py
@@ -5,10 +5,11 @@ en = []
 cr = []
 open('EN.txt', 'w').close()
 open('HR.txt', 'w').close()
-for i in range(1,3):
+for i in range(1,10):
     tmpENG = open('manuals/' + str(i) + '/english.txt', encoding='utf-8').read()
     tmpHR = open('manuals/' + str(i) + '/croatian.txt', encoding='utf-8').read()
     with open("EN.txt", "a", encoding='utf-8') as myfile:
         myfile.write(tmpENG)
     with open("HR.txt", "a", encoding='utf-8') as myfile:
         myfile.write(tmpHR)
+    print(i)
